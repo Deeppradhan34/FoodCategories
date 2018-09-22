efb8cbce5e657927e1c1caee4812cd115fa1b388
DEEP PRADHAN <v-deep.pradhan@yatra.com>
2018-09-22 21:52:47 +0530

    Initial Commit

---
+++ FoodCategories/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FoodCategories
+//
+//  Created by DEEP PRADHAN on 22/09/18.
+//  Copyright © 2018 DEEP PRADHAN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
